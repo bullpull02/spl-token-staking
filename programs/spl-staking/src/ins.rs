7ca5d958eab58a1636f5ec1c863b19a7f482e297
bullpull02 <mykolazbudchak@gmail.com>
2023-06-05 23:52:04 +0000

    smart contract completed basically

---
+++ programs/spl-staking/src/ins.rs
@@ -0,0 +1,217 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+  associated_token::AssociatedToken,
+  token::{TokenAccount, Token, Mint}
+};
+use crate::state::*;
+
+#[derive(Accounts)]
+pub struct InitializeVault<'info> {
+  #[account(mut)]
+  pub authority: Signer<'info>,
+
+  #[account(
+    init,
+    payer = authority,
+    space = Vault::LEN + 8,
+    seeds = [
+      b"vault".as_ref(),
+    ],
+    bump,
+  )]
+  pub vault: Account<'info, Vault>,
+
+  pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateVault<'info> {
+  #[account(mut, address = vault.authority)]
+  pub authority: Signer<'info>,
+
+  #[account(
+    mut,
+    seeds = [
+      b"vault".as_ref(),
+    ],
+    bump = vault.bump,
+  )]
+  pub vault: Account<'info, Vault>,
+}
+
+#[derive(Accounts)]
+pub struct Fund<'info> {
+  #[account(mut)]
+  pub authority: Signer<'info>,
+
+  #[account(
+    mut,
+    seeds = [
+      b"vault".as_ref(),
+    ],
+    bump = vault.bump,
+  )]
+  pub vault: Account<'info, Vault>,
+
+  #[account(address = vault.token_mint)]
+  pub token_mint: Account<'info, Mint>,
+
+  #[account(
+    init_if_needed,
+    payer = authority,
+    associated_token::authority = vault,
+    associated_token::mint = token_mint,
+  )]
+  pub vault_ata: Account<'info, TokenAccount>,
+
+  #[account(
+    associated_token::authority = authority,
+    associated_token::mint = token_mint,
+  )]
+  pub authority_ata: Account<'info, TokenAccount>,
+  
+  pub system_program: Program<'info, System>,
+
+  pub token_program: Program<'info, Token>,
+
+  pub associated_token_program: Program<'info, AssociatedToken>,
+
+  pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct Drain<'info> {
+  #[account(mut, address = vault.authority)]
+  pub authority: Signer<'info>,
+
+  #[account(
+    mut,
+    seeds = [
+      b"vault".as_ref(),
+    ],
+    bump = vault.bump,
+  )]
+  pub vault: Account<'info, Vault>,
+
+  #[account(address = vault.token_mint)]
+  pub token_mint: Account<'info, Mint>,
+
+  #[account(
+    associated_token::authority = vault,
+    associated_token::mint = token_mint,
+  )]
+  pub vault_ata: Account<'info, TokenAccount>,
+
+  #[account(
+    associated_token::authority = authority,
+    associated_token::mint = token_mint,
+  )]
+  pub authority_ata: Account<'info, TokenAccount>,
+
+  pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct CreateUser<'info> {
+  #[account(mut)]
+  pub authority: Signer<'info>,
+
+  #[account(
+    init,
+    payer = authority,
+    space = User::LEN + 8,
+    seeds = [
+      b"user".as_ref(),
+      authority.key.as_ref(),
+    ],
+    bump,
+  )]
+  pub user: Account<'info, User>,
+
+  pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Stake<'info> {
+  #[account(mut, address = user.key)]
+  pub staker: Signer<'info>,
+
+  #[account(
+    mut,
+    seeds = [
+      b"vault".as_ref(),
+    ],
+    bump = vault.bump,
+  )]
+  pub vault: Account<'info, Vault>,
+
+  #[account(
+    mut,
+    seeds = [
+      b"user".as_ref(),
+      staker.key.as_ref(),
+    ],
+    bump = user.bump,
+  )]
+  pub user: Account<'info, User>,
+
+
+  #[account(address = vault.token_mint)]
+  pub token_mint: Account<'info, Mint>,
+
+  #[account(
+    associated_token::authority = vault,
+    associated_token::mint = token_mint,
+  )]
+  pub vault_ata: Account<'info, TokenAccount>,
+
+  #[account(
+    associated_token::authority = staker,
+    associated_token::mint = token_mint,
+  )]
+  pub staker_ata: Account<'info, TokenAccount>,
+
+  pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct Unstake<'info> {
+  #[account(mut, address = user.key)]
+  pub staker: Signer<'info>,
+
+  #[account(
+    mut,
+    seeds = [
+      b"vault".as_ref(),
+    ],
+    bump = vault.bump,
+  )]
+  pub vault: Account<'info, Vault>,
+
+  #[account(
+    mut,
+    seeds = [
+      b"user".as_ref(),
+      staker.key.as_ref(),
+    ],
+    bump = user.bump,
+  )]
+  pub user: Account<'info, User>,
+
+  #[account(address = vault.token_mint)]
+  pub token_mint: Account<'info, Mint>,
+
+  #[account(
+    associated_token::authority = vault,
+    associated_token::mint = token_mint,
+  )]
+  pub vault_ata: Account<'info, TokenAccount>,
+
+  #[account(
+    associated_token::authority = staker,
+    associated_token::mint = token_mint,
+  )]
+  pub staker_ata: Account<'info, TokenAccount>,
+
+  pub token_program: Program<'info, Token>,
+}
--- programs/spl-staking/src/lib.rs
+++ programs/spl-staking/src/lib.rs
@@ -1,15 +1,160 @@
-use anchor_lang::prelude::*;
+mod state;
+mod ins;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Transfer, transfer};
+
+use crate::ins::*;
+
+declare_id!("GfDz6c7PsBK3yFLXAaxmmXsXH1A69j4EFcZ7RUrJMW13");
 
 #[program]
 pub mod spl_staking {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_vault(
+        ctx: Context<InitializeVault>,
+        token_mint: Pubkey,
+        daily_payout_amount: u64,
+    ) -> Result<()> {
+        let vault = &mut ctx.accounts.vault;
+
+        vault.bump = *ctx.bumps.get("vault").unwrap();
+        vault.token_mint = token_mint;
+        vault.daily_payout_amount = daily_payout_amount;
+        vault.authority = ctx.accounts.authority.key();
+        vault.total_reward_amount = 0;
+        vault.total_staked_amount = 0;
+
+        Ok(())
+    }
+
+    pub fn update_vault(
+        ctx: Context<UpdateVault>,
+        token_mint: Pubkey,
+        daily_payout_amount: u64,
+    ) -> Result<()> {
+        let vault = &mut ctx.accounts.vault;
+
+        vault.token_mint = token_mint;
+        vault.daily_payout_amount = daily_payout_amount;
+        
+        Ok(())
+    }
+
+    pub fn create_user(ctx: Context<CreateUser>) -> Result<()> {
+        let user = &mut ctx.accounts.user;
+
+        user.key = ctx.accounts.authority.key();
+        user.bump = *ctx.bumps.get("user").unwrap();
+        user.staked_amount = 0;
+        user.earned_amount = 0;
+
+        Ok(())
+    }
+
+    pub fn fund(ctx: Context<Fund>, amount: u64) -> Result<()> {
+        let vault = &mut ctx.accounts.vault;
+        
+        vault.total_reward_amount = vault.total_reward_amount.checked_add(amount).unwrap();
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.authority_ata.to_account_info(),
+                    to: ctx.accounts.vault_ata.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
+        Ok(())
+    }
+
+    pub fn drain(ctx: Context<Drain>, amount: u64) -> Result<()> {
+        let vault = &mut ctx.accounts.vault;
+        
+        vault.total_reward_amount = vault.total_reward_amount.checked_sub(amount).unwrap();
+        let bump = vault.bump;
+        let seeds = [
+            b"vault".as_ref(),
+            &[bump]
+        ];
+        let signer = &[&seeds[..]];
+
+        transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.vault_ata.to_account_info(),
+                    to: ctx.accounts.authority_ata.to_account_info(),
+                    authority: ctx.accounts.vault.to_account_info(),
+                },
+                signer,
+            ),
+            amount,
+        )?;
+        Ok(())
+    }
+
+    pub fn stake(ctx: Context<Stake>, amount: u64) -> Result<()> {
+        let vault = &mut ctx.accounts.vault;
+        let user = &mut ctx.accounts.user;
+
+        user.update(vault);
+        
+        vault.total_staked_amount = vault.total_staked_amount.checked_add(amount).unwrap();
+        user.staked_amount = user.staked_amount.checked_add(amount).unwrap();
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.staker_ata.to_account_info(),
+                    to: ctx.accounts.vault_ata.to_account_info(),
+                    authority: ctx.accounts.staker.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
+        Ok(())
+    }
+
+    pub fn unstake(ctx: Context<Unstake>, amount: u64, is_claim: bool) -> Result<()> {
+        let vault = &mut ctx.accounts.vault;
+        let user = &mut ctx.accounts.user;
+
+        user.update(vault);
+        let mut amount = amount;
+        if is_claim == true {
+            amount = user.earned_amount;
+            vault.total_reward_amount = vault.total_reward_amount.checked_sub(amount).unwrap();
+            user.earned_amount = 0;
+        } else {
+            vault.total_staked_amount = vault.total_staked_amount.checked_sub(amount).unwrap();
+            user.staked_amount = user.staked_amount.checked_sub(amount).unwrap();
+        }
+
+        let bump = vault.bump;
+        let seeds = [
+            b"vault".as_ref(),
+            &[bump]
+        ];
+        let signer = &[&seeds[..]];
+
+        transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.vault_ata.to_account_info(),
+                    to: ctx.accounts.staker_ata.to_account_info(),
+                    authority: ctx.accounts.vault.to_account_info(),
+                },
+                signer,
+            ),
+            amount,
+        )?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
