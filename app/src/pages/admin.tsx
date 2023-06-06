2d7980fda38e3ae3e2e61fb491b34d9f8a355455
bullpull02 <mykolazbudchak@gmail.com>
2023-06-06 03:03:58 +0000

    integration code completed

--- app/src/pages/admin.tsx
+++ app/src/pages/admin.tsx
@@ -1,15 +1,19 @@
 import { BN } from '@project-serum/anchor';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { PublicKey } from '@solana/web3.js';
+import { TOKEN_MINT } from 'config';
 import useProgram from 'hooks/useProgram';
-import { initializeVault, updateVault } from 'libs/methods';
+import { drain, fund, initializeVault, updateVault } from 'libs/methods';
 import { useState } from 'react';
+
+const tokenMint = new PublicKey(TOKEN_MINT);
 
 export default function Admin() {
   const wallet = useWallet();
   const program = useProgram();
   const [tokenMintAddress, setTokenMintAddress] = useState("");
   const [dailyPayoutAmount, setDailyPayoutAmount] = useState(0);
+  const [amount, setAmount] = useState(0);
 
   const handleInitializeVault = async () => {
     if (!program) return;
@@ -21,6 +25,18 @@ export default function Admin() {
     if (!program) return;
 
     await updateVault(wallet, program, new PublicKey(tokenMintAddress), new BN(dailyPayoutAmount * 1e9));
+  }
+
+  const handleFund = async () => {
+    if (!program) return;
+
+    await fund(wallet, program, tokenMint, amount);
+  }
+
+  const handleDrain = async () => {
+    if (!program) return;
+
+    await drain(wallet, program, tokenMint, amount);
   }
   return (
     <div>
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,9 +1,35 @@
+import { BN } from '@project-serum/anchor';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { PublicKey } from '@solana/web3.js';
+import { TOKEN_MINT } from 'config';
 import useProgram from 'hooks/useProgram';
+import { stake, unstake } from 'libs/methods';
+import { useState } from 'react';
+
+const tokenMint = new PublicKey(TOKEN_MINT);
 
 export default function Home() {
+  const wallet = useWallet();
   const program = useProgram();
+  const [amount, setAmount] = useState(0);
 
-  
+  const handleStake = async () => {
+    if (!program) return;
+
+    await stake(wallet, program, tokenMint, new BN(amount * 1e9));
+  }
+
+  const handleUnstake = async () => {
+    if (!program) return;
+
+    await unstake(wallet, program, tokenMint, new BN(amount * 1e9), false);
+  }
+
+  const handleClaim = async () => {
+    if (!program) return;
+
+    await unstake(wallet, program, tokenMint, new BN(0), true);
+  }
   return (
     <div>
       Home
