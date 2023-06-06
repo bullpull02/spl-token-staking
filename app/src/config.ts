6007dc1cfbc65b0bfb94fbcd419ea4263bc5f6b3
bullpull02 <mykolazbudchak@gmail.com>
2023-06-06 01:13:59 +0000

    dapp added

---
+++ app/src/config.ts
@@ -0,0 +1,9 @@
+export const SOLANA_RPC_URL = "https://patient-empty-rain.solana-mainnet.quiknode.pro/dfba1e28dfc6728a18eef1b0e51e5f4c2db4cc05/";
+export const VAULT_NAME = "breadheads";
+export const COLLECTION_TOTAL_COUNT = 2979;
+export const NFT_CREATOR = "bk5gRo28CvPnDYjWmLvB77h5hPMZp6UDx1Gbg5VTSUG";
+export const VERIFIED_WALLETS = {
+  "3qWq2ehELrVJrTg2JKKERm67cN6vYjm1EyhCEzfQ6jMd": 2,
+  "571RWruF1NSQQw6LfwWXVPNBPiKmVqkv3nqcs3EPfoho": 1,
+};
+export const HYPERSPACE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJGTFdSU0RBTyIsIm5hbWUiOiJIeXBlcnNwYWNlIiwiaWF0IjoxNTE2MjM5MDIyfQ.AGjMmqF3IBpyZ01pZoKfQ_pKLIaHgkIkgMISEWG1KZs";
